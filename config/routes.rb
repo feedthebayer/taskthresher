f9fbaa73ab19a52e74fcbdeb4a4c8ef9da28426d
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-05 19:09:55 -0600

    feat: add user "show" page at root path when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
   resource :session, controller: 'clearance/sessions', only: [:create]
 
-  resources :users, controller: 'clearance/users', only: [:create] do
+  resources :users, only: [:create] do
     resource :password,
       controller: 'clearance/passwords',
       only: [:create, :edit, :update]
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
   get '/register' => 'clearance/users#new', as: 'register'
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'users#show', as: :signed_in_root
+  end
 end
