3c85f9fbd765e73c4a70e85ee14b24e81ca94450
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-07 16:44:06 -0600

    feat: add create task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,23 @@
 class TasksController < ApplicationController
 
   def create
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      # flash[:notice] = "Task saved!"
+    else
+      flash[:error] = "Oops, there was a problem saving the task"
+    end
+
+    redirect_to signed_in_root_path
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < Clearance::UsersController
   def show
     @tasks = current_user.tasks
+    @new_task = Task.new
   end
 end
