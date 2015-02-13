65ece768bfabf393ffd630993ddcbbf6f7580353
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-12 20:50:41 -0600

    refactor: clean up days to deletion method

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   belongs_to :user
 
   def days_to_deletion
-    7 - (Time.now - self.created_at).to_i / 1.day
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
   end
 end
