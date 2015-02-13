776c65b429bea65ad7cc37e89873072a6a1db11e
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-12 20:14:00 -0600

    feat: use ajax for task creation and deletion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,29 @@
 class TasksController < ApplicationController
+  respond_to :html, :js
 
   def create
     @task = current_user.tasks.new(task_params)
+    @new_task = Task.new
 
     if !@task.save
-      flash[:error] = "Oops, there was a problem saving the task"
+      flash.now[:error] = "Oops, there was a problem saving the task"
     end
 
-    redirect_to signed_in_root_path
+    respond_with(@task) do |format|
+      format.html { redirect_to signed_in_root_path }
+    end
   end
 
   def destroy
     @task = current_user.tasks.find(params[:id])
 
     if !@task.destroy
-      flash[:error] = "Oops, there was a problem deleting the task"
+      flash.now[:error] = "Oops, there was a problem deleting the task"
     end
 
-    redirect_to signed_in_root_path
+    respond_with(@task) do |format|
+      format.html { redirect_to signed_in_root_path }
+    end
   end
 
   private
