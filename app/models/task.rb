7b48d9dd102f24b12b4f95020d47ad19db6313d2
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-14 15:19:57 -0600

    refactor: move default_scope to top of file

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :user
   validates :description, presence: true
   validates :user, presence: true
+  default_scope { order('created_at ASC') }
 
   def days_left
     6 - (Time.now.to_date - created_at.to_date).to_i
   end
-
-  default_scope { order('created_at ASC') }
 end
