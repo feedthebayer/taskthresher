84f3b7f632c681a5cdaa3ef23f99e73a0a841aae
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-14 11:59:53 -0600

    feat: add rake task to delete old to-dos(tasks)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :user
 
-  def days_to_deletion
-    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  def days_left
+    6 - (Time.now.to_date - created_at.to_date).to_i
   end
 end
---
+++ lib/tasks/task_thresher.rake
@@ -0,0 +1,6 @@
+namespace :task_thresher do
+  desc "Delete items older than seven days"
+  task delete_items: :environment do
+    Task.where("created_at < ?", Time.now.midnight - 6.days).destroy_all
+  end
+end
