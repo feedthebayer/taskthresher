b9863342bac92874d320854351d5318f9f8a2b37
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-14 14:51:16 -0600

    feat: add flash notice for failed registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < Clearance::UsersController
     @tasks = current_user.tasks
     @new_task = Task.new
   end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      flash.now[:error] = "Uh oh, make sure everything is filled in properly."
+      render template: "users/new"
+    end
+  end
 end
