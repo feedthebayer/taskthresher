1ef40c14a79f52043a4da81a4d9b6d15a49af404
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-02-14 13:59:05 -0600

    fix: require login for task and user controller actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_login
   respond_to :html, :js
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < Clearance::UsersController
+  before_action :require_login
+
   def show
     @tasks = current_user.tasks
     @new_task = Task.new
